db7506adebabc056dfa74fbc3c6e87b15b05f699
filip <filip.oroz9@gmail.com>
2023-10-25 16:11:59 +0200

    button fix and animations added

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const News = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     const [newsData, setNewsData] = useState([]);
-    const [page, setPage] = useState(3);
+    const [page, setPage] = useState(1);
     const batchSize = 1;
     const apiKey = import.meta.env.VITE_NEWS_API;
-
 
     const fetchNews = async () => {
         try {
@@ -16,22 +21,31 @@ export const News = () => {
                 },
             });
 
-            const newArticles = response.data.data.filter(
-                (article) => article.title && article.description && article.language === 'en' && article.image_url
-            );
+            const newArticles = response.data.data.filter((article) => {
+                return (
+                    article.title &&
+                    article.description &&
+                    article.language === 'en' &&
+                    article.image_url
+                );
+            });
 
             if (newArticles.length === 0) {
                 // No more articles to load
                 setPage(-1);
             } else {
-                const newBatch = newArticles.slice(0, batchSize);
-                setNewsData((prevNewsData) => [...prevNewsData, ...newBatch]);
+                if (page === 1) {
+                    // For the first batch, set newsData directly
+                    setNewsData(newArticles);
+                } else {
+                    // For subsequent batches, concatenate new data
+                    setNewsData((prevNewsData) => [...prevNewsData, ...newArticles]);
+                }
             }
         } catch (error) {
             console.error('Error fetching news data:', error);
         }
     };
-
 
     useEffect(() => {
         if (page > 0) {
@@ -44,13 +58,14 @@ export const News = () => {
     };
 
     return (
-        <div className="flex w-full justify-center items-center gradient-bg-education">
+        <div className="flex flex-col items-center gradient-bg-education">
             <div id="news" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
                 {newsData.map((article) => (
                     <div
                         key={article.uuid}
                         className="bg-white rounded-lg p-4 cursor-pointer"
                         onClick={() => window.open(article.url, '_blank')}
+                        data-aos="fade-up"
                     >
                         <h2 className="text-lg font-semibold">{article.title}</h2>
                         <img src={article.image_url} alt="Article" className="w-full h-auto mt-2" />
@@ -59,11 +74,15 @@ export const News = () => {
                 ))}
             </div>
             {page > 0 && (
-                <div className="text-center mt-4">
-                    <button onClick={loadMore} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                <div className="mt-4">
+                    <button
+                        onClick={loadMore}
+                        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+                    >
                         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                             Load More
-                        </span> </button>
+                        </span>
+                    </button>
                 </div>
             )}
         </div>
